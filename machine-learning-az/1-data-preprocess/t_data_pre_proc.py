0772bd385ab73f181b16a593873187a904d1de42
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2018-07-02 19:56:01 -0500

    Updated data preprocessing sample file

---
+++ machine-learning-az/1-data-preprocess/t_data_pre_proc.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  1 18:24:35 2018
+
+@author: tapiw
+"""
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+#Importing the dataet
+dataset = pd.read_csv('Data.csv')
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+# Take care of Missing data -> Use the mean
+from sklearn.preprocessing import Imputer
+
+imputer = Imputer(missing_values='NaN', strategy="mean", axis=0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+#x = pd.DataFrame(data=X)
+
+# Categorical Entries -> Country and Purchased
+# Encode text with 
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+labelEncoder_X = LabelEncoder()
+X[:, 0] = labelEncoder_X.fit_transform(X[:,0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+
+labelEncoder_Y = LabelEncoder()
+y = labelEncoder_Y.fit_transform(y)
+
+#Splitting the data set into training set and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+
+
+
