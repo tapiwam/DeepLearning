a033d43dfd98b0ab2cf490ebd00244826f745274
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2018-07-07 11:09:01 -0500

    Added methods for automatic backward elimination

---
+++ machine-learning-az/3-multiple-linear-regression/backwardElimination_p_values.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  7 11:07:26 2018
+
+@author: tapiw
+"""
+
+import statsmodels.formula.api as sm
+def backwardElimination(x, sl):
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = np.delete(x, j, 1)
+    regressor_OLS.summary()
+    return x
+ 
+SL = 0.05
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+X_Modeled = backwardElimination(X_opt, SL)
