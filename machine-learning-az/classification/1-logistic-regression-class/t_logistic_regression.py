b89104993b19963f7490db66d9d016b9f24c13d5
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2018-07-17 06:27:17 -0500

    Updated ML-AZ notes

---
+++ machine-learning-az/classification/1-logistic-regression-class/t_logistic_regression.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 11 20:09:28 2018
+
+@author: tapiw
+"""
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+# =========================
+#Importing the dataset
+dataset = pd.read_csv('Data.csv')
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+# =========================
+#Splitting the data set into training set and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# =========================
+# Feature scaling
+"""
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+"""
+
+# =========================
+# Modelling
